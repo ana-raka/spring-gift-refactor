7ea49ba82e390c4b7e232ecd040209e7543397c5
UiChan Lee <junglkls1234@gmail.com>
2025-01-18 12:37:26 +0900

    !BREAKING CHANGE: view 렌더링과 데이터 처리가 하나의 controller에 존재, 이를 별도의 controller로 분리

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,19 +2,14 @@ package gift.controller;
 
 import gift.dto.productDto.ProductDto;
 import gift.dto.productDto.ProductResponseDto;
-import gift.model.product.Product;
+import gift.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import gift.service.ProductService;
-
 
 @RequestMapping("/api/products")
 @Controller
@@ -34,6 +29,13 @@ public class ProductController {
         return ResponseEntity.ok().body(productResponseDto);
     }
 
+    @DeleteMapping("/{productId}")
+    @Operation(summary = "등록된 상품을 삭제", description = "등록된 상품을 삭제할 때 사용하는 API")
+    public String deleteProduct(@PathVariable Long productId){
+        productService.DeleteProduct(productId);
+        return "productManage";
+    }
+
     @PutMapping("/{productId}")
     @Operation(summary = "상품을 업데이트", description = "등록된 상품을 업데이트 할 때 사용하는 API")
     public ResponseEntity<ProductResponseDto> updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductDto productDto) {
@@ -46,30 +48,5 @@ public class ProductController {
     public ResponseEntity<ProductResponseDto> getProductById(@Valid @PathVariable Long productId) {
         ProductResponseDto productResponseDto = productService.getProductById(productId);
         return ResponseEntity.ok().body(productResponseDto);
-    }
-
-    @GetMapping
-    @Operation(summary = "등록된 전체 상품 리스트 조회", description = "전체 상품 목록을 조회할 때 사용하는 API")
-    public String getProductList(@RequestParam(defaultValue = "0") int page, Model model) {
-        Page<Product> productPage = productService.getAllProducts(PageRequest.of(page, 20));
-        model.addAttribute("products", productPage.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", productPage.getTotalPages());
-        return "productManage";
-    }
-
-    @DeleteMapping("/{productId}")
-    @Operation(summary = "등록된 상품을 삭제", description = "등록된 상품을 삭제할 때 사용하는 API")
-    public String deleteProduct(@PathVariable Long productId){
-        productService.DeleteProduct(productId);
-        return "productManage";
-    }
-
-    @GetMapping("/edit/{productId}")
-    @Operation(summary = "상품 수정 페이지로 이동", description = "상품 수정 페이지로 이동할 때 사용하는 API")
-    public String moveToEditProduct(@PathVariable Long productId, Model model) {
-        Product product = productService.selectProduct(productId);
-        model.addAttribute("product", product);
-        return "editProduct";
     }
 }
---
+++ src/main/java/gift/controller/ProductWebController.java
@@ -0,0 +1,41 @@
+package gift.controller;
+
+import gift.model.product.Product;
+import gift.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/products")
+public class ProductWebController {
+    private final ProductService productService;
+
+    public ProductWebController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    @Operation(summary = "전체 상품 목록 페이지 조회", description = "등록된 상품 전체 조회 후 View 렌더링 후 반환합니다.")
+    public String showProductListPage(@RequestParam(defaultValue = "0") int page, Model model) {
+        Page<Product> productPage = productService.getAllProducts(PageRequest.of(page, 20));
+        model.addAttribute("products", productPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", productPage.getTotalPages());
+        return "productManage";
+    }
+
+    @GetMapping("/{productId}")
+    @Operation(summary = "상품 수정 페이지 조회", description = "ProductId에 등록된 상품이 있는 경우 상품을 수정하는 페이지를 반환합니다.")
+    public String showEditProductPage(@PathVariable Long productId, Model model) {
+        Product product = productService.selectProduct(productId);
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+}
